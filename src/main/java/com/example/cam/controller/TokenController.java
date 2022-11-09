cf7232dfd0604a570c9a039a4bd52db74a13c6e6
David Nguyen <66445509+n-u-david@users.noreply.github.com>
2022-11-09 17:12:41 -0600

    Migrate token logic

--- src/main/java/com/example/cam/controller/TokenController.java
+++ src/main/java/com/example/cam/controller/TokenController.java
@@ -17,38 +17,12 @@ public class TokenController {
     @GetMapping("/eat")
     public String getEat() {
         ExternalToken eatToken = new ExternalToken();
-        eatToken.setHeader("{ \"alg\" : \"HS256\", \"typ\": \"EAT\"}");
-        eatToken.setBody("{\"sub\" : \"1234567898\", \"name\" : \"John Doe\", \"iat\": \""+ getTokenTime()+ "\" }");
-        eatToken.setSignature("testSignature");
-
-        return base64Encode(eatToken.getHeader())+"."+
-                base64Encode(eatToken.getBody())+"."+
-                base64Encode(eatToken.getSignature());
+        return eatToken.getToken();
     }
 
     @GetMapping("/iat")
     public String getIat() {
         InternalToken iatToken = new InternalToken();
-        iatToken.setHeader("{ \"alg\" : \"HS256\", \"typ\": \"IAT\"}");
-        iatToken.setBody("{\"sub\" : \"1234567898\", \"name\" : \"John Doe\", \"iat\": \""+ getTokenTime()+ "\" }");
-        iatToken.setSignature("testSignature");
-
-        return base64Encode(iatToken.getHeader())+"."+
-                base64Encode(iatToken.getBody())+"."+
-                base64Encode(iatToken.getSignature());
-    }
-
-    private String getTokenTime(){
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        System.out.println(timestamp);
-
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(timestamp.getTime());
-        cal.add(Calendar.MINUTE, 60);
-        return String.valueOf(cal.getTimeInMillis());
-    }
-
-    private String base64Encode(String input){
-        return Base64.getEncoder().encodeToString(input.getBytes());
+        return iatToken.getToken();
     }
 }
--- src/main/java/com/example/cam/model/ExternalToken.java
+++ src/main/java/com/example/cam/model/ExternalToken.java
@@ -2,10 +2,36 @@ package com.example.cam.model;
 
 import lombok.*;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Date;
 
 @Data
 public class ExternalToken implements Serializable {
     String header;
     String body;
     String signature;
+
+    private String base64Encode(String input){
+        return Base64.getEncoder().encodeToString(input.getBytes());
+    }
+
+    public String getToken(){
+        this.setHeader("{ \"alg\" : \"HS256\", \"typ\": \"EAT\"}");
+        this.setBody("{\"sub\" : \"1234567898\", \"name\" : \"John Doe\", \"iat\": \""+ getTokenTime()+ "\" }");
+        this.setSignature("externalTestSignature");
+        return base64Encode(header)+"."+
+                base64Encode(body)+"."+
+                base64Encode(signature);
+    };
+    private String getTokenTime(){
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        System.out.println(timestamp);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(timestamp.getTime());
+        cal.add(Calendar.MINUTE, 60);
+        return String.valueOf(cal.getTimeInMillis());
+    }
 }
